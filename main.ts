00c612969f5f8796f6e74037ce7467e0e77eed19
AdjeH <86462820+AdjeH@users.noreply.github.com>
2021-06-27 15:37:16 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,18 +1,14 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 3) {
-        RobotCar_Keyestudio.Motors.move(Snelheid_vooruit)
+        RobotCar_Keyestudio.Motors.move(20)
     } else if (receivedNumber == 4) {
-        RobotCar_Keyestudio.Motors.move(-10)
+        RobotCar_Keyestudio.Motors.move(-20)
     } else if (receivedNumber == 1) {
-        RobotCar_Keyestudio.Motors.spin(-10)
+        RobotCar_Keyestudio.Motors.spin(-20)
     } else if (receivedNumber == 2) {
-        RobotCar_Keyestudio.Motors.spin(10)
-    } else if (receivedNumber == 5) {
-        Snelheid_vooruit += 10
-    } else if (receivedNumber == 6) {
-        Snelheid_vooruit += -10
+        RobotCar_Keyestudio.Motors.spin(20)
+    } else {
+        RobotCar_Keyestudio.Motors.stop()
     }
 })
-let Snelheid_vooruit = 0
 radio.setGroup(128)
-Snelheid_vooruit = 10
