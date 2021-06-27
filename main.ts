72f8210b1730a60d5f4ffa1b30e553c84c4ca5a7
AdjeH <86462820+AdjeH@users.noreply.github.com>
2021-06-27 19:47:01 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 3) {
-        RobotCar_Keyestudio.Motors.move(20)
+        RobotCar_Keyestudio.Motors.move(30)
     } else if (receivedNumber == 4) {
-        RobotCar_Keyestudio.Motors.move(-20)
+        RobotCar_Keyestudio.Motors.move(-30)
     } else if (receivedNumber == 1) {
         RobotCar_Keyestudio.Motors.spin(-20)
     } else if (receivedNumber == 2) {
