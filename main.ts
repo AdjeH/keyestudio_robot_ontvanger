293ef13ac6cc7d035db05659011da3d2f2d7adb2
AdjeH <86462820+AdjeH@users.noreply.github.com>
2021-06-27 15:07:13 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 3) {
+        RobotCar_Keyestudio.Motors.move(Snelheid_vooruit)
+    } else if (receivedNumber == 4) {
+        RobotCar_Keyestudio.Motors.move(-10)
+    } else if (receivedNumber == 1) {
+        RobotCar_Keyestudio.Motors.spin(-10)
+    } else if (receivedNumber == 2) {
+        RobotCar_Keyestudio.Motors.spin(10)
+    } else if (receivedNumber == 5) {
+        Snelheid_vooruit += 10
+    } else if (receivedNumber == 6) {
+        Snelheid_vooruit += -10
+    }
 })
+let Snelheid_vooruit = 0
+radio.setGroup(128)
+Snelheid_vooruit = 10
